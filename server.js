b5322099a39eeff5a32dc68b7c6367735de6258e
Yash Naik <naikyash535@gmail.com>
2025-02-17 17:27:08 +0530

    env change

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
-
 require("dotenv").config();
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 //regular middleware
 app.use(express.json());
@@ -17,10 +17,6 @@ const postRouter = require("./Routes/postRoutes");
 app.use("/api", userRouter);
 app.use("/api", postRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hi from youtube live");
-});
-
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("server is running on port 3000");
 });
